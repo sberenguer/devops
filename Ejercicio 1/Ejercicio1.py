3a3b72ade141d20af14e6606ae84b834f3589df3
sberenguer <sberenguer85@gmail.com>
2020-02-29 13:06:08 +0100

    Cambio de nombre en los metodos + enunciado ejercicio

--- Ejercicio 1/Ejercicio1.py
+++ Ejercicio 1/Ejercicio1.py
@@ -1,37 +1,42 @@
 #Ejercicio1 
 
-def tipoDeNumero(inputList):
-    print("Lista de entrada: "+str(inputList))
-    for numero in inputList:
-        calculaDivisores(numero)
-        #print("Divisores calculados para "+ str(numero))
+#Dada una lista de numeros como parametro, para cada numero se indica si
+#es perfecto, abundante o defectivo
+#   Un número perfecto es aquel que es igual a la suma de sus divisores
+#propios positivos, excluyéndose a sí mismo. Por ejemplo 6 = 1+2+3
+#   Un número abundante es aquel que la suma de los divisores propios es
+#mayor que el número.
+#   Un número defectivo es aquel que la suma de los divisores propios es
+#menor que el número.
+
+def tipoDeNumero(lista):
+    print("Lista de números de entrada: "+str(lista))
+    for numero in lista:
+        calculaTipoDeNumero(numero)
        
 
-def calculaDivisores(n):
-    print("Calculando divisores de " + str(n))
-    
-    divisores=[]
+def calculaTipoDeNumero(n):        
     if n == 0:
-        divisores.append(n)
+        print("El 0 no tiene divisores")
     else:
-        for i in range(1, n+1):
+        divisores=[]
+        sumatorio = 0
+        for i in range(1, n):
             if n%i == 0:
                 divisores.append(i)
-    print(divisores)        
-    return divisores
-    
+                sumatorio +=i
 
-def esPerfecto(n, divisores):
-    print("Calculando si " + str(n) + " es perfecto...")
+        #print("Divisores propios de "+ str(n) + ": " + str(divisores))
+        
+        esPerfectoAbundanteODefectivo(n, sumatorio)
 
+#A partir de un numero        
+def esPerfectoAbundanteODefectivo(n, sumatorio):
+    if sumatorio == n:
+        print("El número "+ str(n) + " es perfecto")
+        
+    elif sumatorio < n:
+        print("El número "+ str(n) + " es defectivo")
 
-
-def esAbundante(n, divisores):
-    print("Calculando si " + str(n) + " es abundante...")
-
-
-def esDefectivo(n, divisores):
-    print("Calculando si " + str(n) + " es defectivo...")
-
-print("Ejercicio 1")
-#tipoDeNumero([1,2,3])
+    else:
+        print("El número "+ str(n) + " es abundante")
